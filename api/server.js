51e73321053ccc2f6b7c9955efa0efa55f9fcdf3
Luiz-Rodrigues05 <luiz.rodrigues@viitra.com>
2025-06-21 23:17:25 -0300

    refactor(authentication): melhorias no retorno de usuário do login
    - A função do login agora retorna diretamente os dados do usuário ao invés de um token dummy. Idealmente, implementações futuras devem gerar um token JWT para autenticação. Isso facilita o desenvolvimento e melhora a segurança.
    
    feat(api): adicione endpoint para buscar cursos de usuário
    
    - Um novo endpoint foi adicionado para buscar os cursos de um usuário com base em seu ID. A rota /user/:userID/courses agora retorna os cursos onde o usuário é o criador. Além disso, foram feitas verificações adicionais para garantir que IDs válidos sejam fornecidos.

--- api/server.js
+++ api/server.js
@@ -21,12 +21,38 @@ server.post('/login', (req, res) => {
       return res.status(400).json({ message: 'Senha incorreta' });
     }
   
-    // Se email e senha estiverem corretos
-    const session = { user_id: user.id, token: 'dummy-token' };
+    // [REFACTOR]
+    // Idealmente, aqui deveria gerar um token JWT ou algo similar para autenticação
+    // Mas para este exemplo, vou apenas retornar os dados do usuário para get fácil
+    const session = { id: user.id, name: user.name, email: user.email };
     router.db.get('sessions').push(session).write();
-    res.status(200).json({ message: 'Login bem-sucedido', token: session.token });
+    res.status(200).json(session);
   });
   
+// Endpoint para buscar os cursos de um usuário
+server.post('/user/:userID/courses', (req, res) => {
+  const { userID } = req.params;
+
+  // Converte o userID para número, pois no db.json o id é um número
+  const id = parseInt(userID, 10);
+
+  if (isNaN(id)) {
+    return res.status(400).json({ message: 'O ID do usuário deve ser um número.' });
+  }
+
+  const user = router.db.get('users').find({ id }).value();
+
+  // Verifica se o usuário existe
+  if (!user) {
+    return res.status(404).json({ message: 'Usuário não encontrado' });
+  }
+
+  // Filtra os cursos onde o usuário é o criador
+  // [REFACTOR] falta caso de indicador
+  const courses = router.db.get('courses').filter({ creator_id: id }).value();
+
+  res.status(200).json(courses);
+});
 
 // Usar o roteador do json-server
 server.use(router)
---
+++ client/src/services/course/index.js
@@ -0,0 +1,18 @@
+import request from '../../services/axios';
+
+const baseURL = '/courses';
+
+export const getCourses = async (userID) => {
+  try {
+    const response = await request('POST', `/user/${userID}${baseURL}`, {
+      userID
+    });
+
+    return response;
+  } catch (error) {
+    if (error.response) {
+      throw new Error(error.response.data?.message || 'Erro ao obter cursos. Tente novamente.');
+    }
+    throw new Error(error.message || 'Erro desconhecido.');
+  }
+};
